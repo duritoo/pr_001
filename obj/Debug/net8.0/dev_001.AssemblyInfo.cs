ee6c2468a6b94e42dd88a3cc8a4095c655717f88
DUSHYANT <dushyant5er@gmail.com>
2025-08-27 18:34:06 +0530

    commit 1 yml file add

--- obj/Debug/net8.0/dev_001.AssemblyInfo.cs
+++ obj/Debug/net8.0/dev_001.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dev_001")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5056fdc974384de6caccf6a6a9b5ec5f6ce352d")]
 [assembly: System.Reflection.AssemblyProductAttribute("dev_001")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dev_001")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
