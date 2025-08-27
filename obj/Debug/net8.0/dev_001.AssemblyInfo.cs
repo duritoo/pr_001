190d498107a41745339fe98195516b220e72ad31
DUSHYANT <dushyant5er@gmail.com>
2025-08-27 19:20:39 +0530

    docker hosting complete

--- obj/Debug/net8.0/dev_001.AssemblyInfo.cs
+++ obj/Debug/net8.0/dev_001.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dev_001")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5056fdc974384de6caccf6a6a9b5ec5f6ce352d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66e8a23759f8c2df9ee6261c36ef68972220536a")]
 [assembly: System.Reflection.AssemblyProductAttribute("dev_001")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dev_001")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
